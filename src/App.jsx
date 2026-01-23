5a619f87f65bbb8cef81f730150eb8c632b5a8d1
Prabhjot Singh Brar <prabhjotsinghbrar39@gmail.com>
2026-01-23 10:50:58 +0530

    add knowledge form with dynamic state update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import { useState } from "react";
 import KnowledgeCard from "./knowledgeCard";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("Video");
+
+   function handleAddKnowledge() {
+  if (!title.trim()) return;
+
+  const newItem = {
+    id: Date.now(),
+    title,
+    type,
+  };
+
+  setKnowledgeItems([...knowledgeItems, newItem]);
+  setTitle("");
+  setType("Video");
+}
+
   const [knowledgeItems, setKnowledgeItems] = useState([
     { id: 1, title: "React Hooks Explained", type: "Video" },
     { id: 2, title: "System Design Podcast", type: "Podcast" },
@@ -13,6 +30,36 @@ function App() {
       <h1 className="text-2xl font-bold mb-6">
         Knowledge Vault
       </h1>
+
+      <div className="bg-white p-4 rounded-xl shadow-md mb-6">
+     <div className="flex gap-4">
+    <input
+      type="text"
+      placeholder="Enter knowledge title"
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      className="flex-1 border border-slate-300 rounded-lg px-3 py-2"
+    />
+
+     <select
+      value={type}
+      onChange={(e) => setType(e.target.value)}
+      className="border border-slate-300 rounded-lg px-3 py-2"
+     >
+      <option>Video</option>
+      <option>Article</option>
+      <option>Podcast</option>
+      <option>Note</option>
+    </select>
+
+    <button
+      onClick={handleAddKnowledge}
+      className="bg-blue-600 text-white px-4 py-2 rounded-lg"
+    >
+      Add
+    </button>
+  </div>
+</div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {knowledgeItems.map((item) => (
